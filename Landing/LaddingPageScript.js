63b86e292134c231b8816af68a1e0cd97d8a0480
Capyvara13 <tiagorafleite@gmail.com>
2025-02-27 07:22:34 -0300

    Tche

---
+++ Landing/LaddingPageScript.js
@@ -0,0 +1,44 @@
+const bar1 = document.getElementById("bar1");
+const bar2 = document.getElementById("bar2");
+const bar3 = document.getElementById("bar3");
+const mobileBars = document.getElementById("mobileBars");
+
+var isClicked = false;
+
+function clickAniMenu() {
+    if (!isClicked) {
+        bar1.classList.remove("bar1AnC");
+        bar2.classList.remove("bar2AnC");
+        bar3.classList.remove("bar3AnC");
+
+        bar1.classList.add("bar1AnO");
+        bar2.classList.add("bar2AnO");
+        bar3.classList.add("bar3AnO");
+        bar1.style.transform = "rotate(50deg)";
+        bar1.style.top = "3vh";
+        bar2.style.width = "0";
+        bar3.style.transform = "rotate(-50deg)";
+        bar3.style.top = "-0.8vh";
+
+        console.log("click aberto"); 
+    } else if (isClicked) {
+        bar1.classList.remove("bar1AnO");
+        bar2.classList.remove("bar2AnO");
+        bar3.classList.remove("bar3AnO");
+
+        bar1.classList.add("bar1AnC");
+        bar2.classList.add("bar2AnC");
+        bar3.classList.add("bar3AnC");
+        bar1.style.transform = "rotate(0deg)";
+        bar1.style.top = "1vh";
+        bar2.style.width = "6vw";
+        bar3.style.transform = "rotate(0deg)";
+        bar3.style.top = "1vh";
+        
+        console.log("click fechado"); 
+    }
+
+    isClicked = !isClicked;
+}
+
+mobileBars.addEventListener("click", clickAniMenu); 
--- README.md
+++
@@ -1,3 +0,0 @@
-# Itens para fazer!!
-    - Checar a responsividade do Login Page;
-    - Responsividade da Página Principal
